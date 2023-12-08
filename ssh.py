35b631fc1df181dd2fab3146d4e2d401b388720d
abhishekpandey127 <113596375+abhishekpandey127@users.noreply.github.com>
2023-12-07 20:59:07 -0500

    cd /scratch and sbatch implemented

--- ssh.py
+++ ssh.py
@@ -5,12 +5,10 @@ import time
 
 json_file_path = 'creds.json'
 
-
 # Function to connect to an SSH server
 def ssh_connect(ssh_client, hostname, username, password):
     known_hosts_file = os.path.expanduser('~/.ssh/known_hosts')
     host_key_known = False
-
     try:
         with open(known_hosts_file, 'r') as file:
             for line in file:
@@ -31,35 +29,35 @@ def ssh_connect(ssh_client, hostname, username, password):
 
 json_file_path = 'creds.json'
 
-# Create an SSH client instance
 ssh = paramiko.SSHClient()
 ssh.load_system_host_keys()
 
 try:
     with open(json_file_path, 'r') as file:
         creds = json.load(file)
-
-    # First SSH into the gateway server
+    
+    #Connecting to Gateway
     print("Connecting to gateway server...")
     ssh_connect(ssh, 'gw.hpc.nyu.edu', creds['username'], creds['password'])
-
-    # Use the gateway server's session to connect to the target server
+    print("Connection to gateway successful \n")
+    
+    #Connecting to Greene
     print("Connecting to target server...")
     target_ssh = ssh.invoke_shell()
     target_ssh.send(f'ssh {creds["username"]}@greene.hpc.nyu.edu\n')
     time.sleep(5)
-    # Wait for the password prompt and send the password
-    # while not target_ssh.recv_ready():  # Wait for the server to be ready
     target_ssh.send(creds['password'] + '\n')
-    print("Connection to target server successful")
+    print("Connection to target server successful\n")
 
-    sbatch_command = f'sbatch /scratch/{creds["username"]}/test/hello-python.sbatch'
+    #Changing to scratch directory
+    target_ssh.send(f'cd /scratch/{creds["username"]}/test' + '\n')
     time.sleep(5)
-    # sbatch_command = 'cd /scratch/'+creds['username']+'/mro_env/jobs && sbatch job.slurm'
 
-    target_ssh.send(sbatch_command + '\n')
-    # while not target_ssh.recv_ready():  # Wait for the server to be ready
-    time.sleep(2)
+    #Running sbatch file
+    target_ssh.send('sbatch hello-python.sbatch' + '\n')
+    time.sleep(5)
+    
+    #Printing outputs
     output = target_ssh.recv(1024)
     print("Output:")
     print(output.decode('utf-8'))
